c255e6e25cd09f5295fa83243036680145179830
bluehole333 <bluehole333@foxmail.com>
2019-09-22 11:08:35 +0800

    代码重构

--- diff.py
+++ diff.py
@@ -1,5 +1,9 @@
 #!/usr/bin python
 # coding=utf-8
+"""
+代码发布脚本
+获取当前tag列表:os.popen("cd /home/webapps/huisuproject/huisu/;git tag;").read().split('\n')
+"""
 import filecmp
 import md5
 import os
@@ -8,10 +12,21 @@ import time
 import pdb
 import commands
 
-FILE_PATH = "/tmp/project/"
-APP_NAME = "xxxx/"
+# FILE_PATH = "/Users/machiko/secret/"
+# APP_NAME = "test2/"
+#
+# PRD_FILE_PATH = "/Users/machiko/Desktop/"
 
-PRD_FILE_PATH = "/home/liuxin/project/"
+# FILE_PATH: 代码临时存放目录 用于比对文件
+# PRD_FILE_PATH: 生产环境代码路径
+GIT_URL = u"git@xxxxx:/data/git/xxxxx.git"
+FILE_PATH = u"/tmp/xxxxx/"
+PRD_FILE_PATH = u"/home/webapps/xxxxx/"
+APP_NAME = u"xxxx/"
+
+# 忽略检查文件夹 和 扩展名
+FILTERS = ['.git', 'front_end', 'migrations', '.python-eggs', 'weixin_access_token.yaml', '.pki', 'analysis_charts']
+FILTERS_EXTENSION = ['.git', '.pyc', '.sqlite3', '.yaml']
 
 
 def file_cmp(file1, file2):
@@ -19,13 +34,24 @@ def file_cmp(file1, file2):
 
 
 def compare_file(file1, file2):
+
     return md5.new(file1.read()).digest() == md5.new(file2.read()).digest()
+
+
+def file_pass(path):
+    status = False
+    for item in FILTERS:
+        if item in path:
+            status = True
+            break
+
+    return status
 
 
 def check_file(version):
     os.chdir(FILE_PATH)
     if not os.path.exists(APP_NAME):
-        os.system("git clone ssh://git@xxxxxxxx/home/git/xxxxx.git")
+        os.system("git clone %s" % GIT_URL)
 
     os.chdir(os.path.join(FILE_PATH, APP_NAME))
     commands.getstatusoutput("git pull")
@@ -39,51 +65,57 @@ def check_file(version):
 
     return True
 
-
 _add = []
 _update = []
-FILTERS = ['.git', '.pyc']
+_del = []
 
 
 def main(version):
-    print(u"本次更新版本:", version)
+    print u"本次更新版本:", version
     if not check_file(version):
-        print(u"%s版本不存在" % version)
+        print u"%s版本不存在" % version
         return
 
     # 以线上代码为参照 去比对最新代码库
     for root, dirs, files in os.walk(os.path.join(PRD_FILE_PATH, APP_NAME)):
         path = root.split(PRD_FILE_PATH)[1]
+
+        if file_pass(path):
+            continue
+
         for dir in dirs:
+            if file_pass(dir):continue
+
             if not os.path.exists(os.path.join(FILE_PATH, path, dir)):
                 _add.append(os.path.join(FILE_PATH, path, dir))
-
-        if dirs == '.git':
-            continue
 
         for f in files:
             cmp_path = os.path.join(FILE_PATH, path, f)
 
             if os.path.exists(os.path.join(FILE_PATH, path, f)):
-                # if f in "manage.py":
-                #     pdb.set_trace()
-                #     print "xxxxxxxxxxxxxx"
                 if not compare_file(open(os.path.join(PRD_FILE_PATH, path, f), 'r'), open(cmp_path, 'r')):
                     _update.append(cmp_path)
             else:
-                if not os.path.splitext(cmp_path)[1] in FILTERS:
-                    _add.append(cmp_path)
+                if not os.path.splitext(cmp_path)[1] in FILTERS_EXTENSION:
+                    _del.append(cmp_path)
 
-    print(u"\n新添加文件:", len(_add))
-    for item in _add:
-        print(item)
-
-    print(u"\n本次更新文件:")
+    print u"\n更新文件:", len(_update)
     for item in _update:
-        print(item)
-    print("\n")
+        print "\033[1;32;40m %s \033[0m" % item
+
+    print u"\n新添加文件:", len(_add)
+    for item in _add:
+        print "\033[1;33;40m +%s \033[0m" % item
+
+    print "\n"
+
+    print u"\n删除文件:", len(_del)
+    for item in _del:
+        print "\033[1;31;40m -%s \033[0m" % item
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         main(sys.argv[1])
+
+
